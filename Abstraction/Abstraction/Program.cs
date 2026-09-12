9351a346b4ccd0865d03f8f19c1d3614e6077168
Sufiyan Shahid <sufiyanshahid144@gmail.com>
2026-09-12 16:48:32 +0500

    initial commit

---
+++ Abstraction/Abstraction/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TVremote vremote = new TVremote();
+            vremote.TurnOn();
+
+            ACremote aCremote = new ACremote();
+            aCremote.TurnOn();
+        }
+    }
+}
